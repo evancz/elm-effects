83cc4e51543c1690abf3f7dfc0e69cd8d3832f18
Evan Czaplicki <info@elm-lang.org>
2015-07-30 11:49:30 -0700

    Basic example with effects

--- examples/1/Counter.elm
+++ examples/1/Counter.elm
@@ -1,53 +1,113 @@
 module Counter where
 
+import Debug
 import Html exposing (..)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, src)
 import Html.Events exposing (onClick)
-import Tea exposing (Transaction, done)
+import Http
+import Json.Decode as Json
+import Task
+import Tea exposing (Transaction, done, request, Never)
 
 
 main = output.html
 
 output =
-  Tea.start { model = 0, init = [], update = update, view = view }
+  Tea.start { init = init "funny cats", update = update, view = view }
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  output.tasks
 
 
 -- MODEL
 
-type alias Model = Int
+type alias Model =
+    { topic : String
+    , image : String
+    }
+
+
+init : String -> Transaction Message Model
+init topic =
+  request
+    (getRandomImage topic)
+    { topic = topic, image = "http://giphy.com/gifs/bored-alice-in-wonderland-meh-ZXKZWB13D6gFO" }
 
 
 -- UPDATE
 
-type Message = Increment | Decrement
+type Message
+    = Next
+    | NewImage (Maybe String)
+
 
 update : Message -> Model -> Transaction Message Model
-update action model =
-  case action of
-    Increment ->
-      done (model + 1)
+update msg model =
+  case Debug.log "msg" msg of
+    Next ->
+      request
+        (getRandomImage model.topic)
+        model
 
-    Decrement ->
-      done (model - 1)
+    NewImage maybeUrl ->
+      done
+        { model |
+            image <- Maybe.withDefault model.image maybeUrl
+        }
 
 
 -- VIEW
 
+(=>) = (,)
+
 view : Signal.Address Message -> Model -> Html
 view address model =
-  div []
-    [ button [ onClick address Decrement ] [ text "-" ]
-    , div [ countStyle ] [ text (toString model) ]
-    , button [ onClick address Increment ] [ text "+" ]
+  div [ style [ "width" => "200px" ] ]
+    [ h2 [headerStyle] [text model.topic]
+    , div [imgStyle model.image] []
+    , button [ onClick address Next ] [ text "More Please!" ]
     ]
 
 
-countStyle : Attribute
-countStyle =
+headerStyle : Attribute
+headerStyle =
   style
-    [ ("font-size", "20px")
-    , ("font-family", "monospace")
-    , ("display", "inline-block")
-    , ("width", "50px")
-    , ("text-align", "center")
+    [ "width" => "200px"
+    , "text-align" => "center"
     ]
+
+
+imgStyle : String -> Attribute
+imgStyle url =
+  style
+    [ "display" => "inline-block"
+    , "width" => "200px"
+    , "height" => "200px"
+    , "background-position" => "center center"
+    , "background-size" => "cover"
+    , "background-image" => ("url('" ++ url ++ "')")
+    ]
+
+
+-- EFFECTS
+
+randomUrl : String -> String
+randomUrl topic =
+  Http.url "http://api.giphy.com/v1/gifs/random"
+    [ "api_key" => "dc6zaTOxFJmzC"
+    , "tag" => topic
+    ]
+
+
+getRandomImage : String -> Tea.Effect Message
+getRandomImage topic =
+  Http.get decodeImageUrl (randomUrl topic)
+    |> Task.toMaybe
+    |> Task.map (NewImage << Debug.log "result")
+    |> Tea.task
+
+
+decodeImageUrl : Json.Decoder String
+decodeImageUrl =
+  Json.at ["data", "image_url"] Json.string
