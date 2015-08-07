b81ccc6ead405b5dc3765e465d264f8ed10ae968
Evan Czaplicki <info@elm-lang.org>
2015-08-07 15:21:19 -0700

    Update example 3

--- examples/3/RandomGifList.elm
+++ examples/3/RandomGifList.elm
@@ -1,12 +1,13 @@
 module RandomGifList where
 
+import Effects as Fx exposing (Effects, map, batch, Never)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode as Json
 import Start
 import Task
-import Transaction exposing (Transaction, done, tag, with, Never)
+
 import RandomGif as Gif
 
 
@@ -37,9 +38,9 @@ type alias Model =
     }
 
 
-init : Transaction Message Model
+init : (Model, Effects Message)
 init =
-  done { topic = "", gifList = [], uid = 0 }
+    ( Model "" [] 0, Fx.none )
 
 
 -- UPDATE
@@ -50,35 +51,47 @@ type Message
     | SubMsg Int Gif.Message
 
 
-update : Message -> Model -> Transaction Message Model
+update : Message -> Model -> (Model, Effects Message)
 update message model =
-  case message of
-    Topic topic ->
-      done { model | topic <- topic }
+    case message of
+        Topic topic ->
+            ( { model | topic <- topic }
+            , Fx.none
+            )
 
-    Create ->
-      with
-        (tag (SubMsg model.uid) (Gif.init model.topic))
-        (\newRandomGif ->
-            done { model |
-                topic <- "",
-                uid <- model.uid + 1,
-                gifList <- model.gifList ++ [(model.uid, newRandomGif)]
-            }
-        )
+        Create ->
+            let
+                (newRandomGif, fx) =
+                    Gif.init model.topic
 
-    SubMsg msgId msg ->
-      let subUpdate (id, randomGif) =
-            if id == msgId then
-                with
-                  (tag (SubMsg id) (Gif.update msg randomGif))
-                  (\newRandomGif -> done (id, newRandomGif))
-            else
-                done (id, randomGif)
-      in
-        with
-          (Transaction.list (List.map subUpdate model.gifList))
-          (\gifList -> done { model | gifList <- gifList })
+                newModel =
+                    Model "" (model.gifList ++ [(model.uid, newRandomGif)]) (model.uid + 1)
+            in
+                ( newModel
+                , map (SubMsg model.uid) fx
+                )
+
+        SubMsg msgId msg ->
+            let
+                subUpdate ((id, randomGif) as entry) =
+                    if id == msgId then
+                        let
+                            (newRandomGif, fx) = Gif.update msg randomGif
+                        in
+                            ( (id, newRandomGif)
+                            , map (SubMsg id) fx
+                            )
+                    else
+                        (entry, Fx.none)
+
+                (newGifList, fxList) =
+                    model.gifList
+                        |> List.map subUpdate
+                        |> List.unzip
+            in
+                ( { model | gifList <- newGifList }
+                , batch fxList
+                )
 
 
 -- VIEW
@@ -88,43 +101,43 @@ update message model =
 
 view : Signal.Address Message -> Model -> Html
 view address model =
-  div []
-    [ input
-        [ placeholder "What kind of gifs do you want?"
-        , value model.topic
-        , onEnter address Create
-        , on "input" targetValue (Signal.message address << Topic)
-        , inputStyle
+    div []
+        [ input
+            [ placeholder "What kind of gifs do you want?"
+            , value model.topic
+            , onEnter address Create
+            , on "input" targetValue (Signal.message address << Topic)
+            , inputStyle
+            ]
+            []
+        , div [ style [ "display" => "flex", "flex-wrap" => "wrap" ] ]
+            (List.map (elementView address) model.gifList)
         ]
-        []
-    , div [ style [ "display" => "flex", "flex-wrap" => "wrap" ] ]
-        (List.map (elementView address) model.gifList)
-    ]
 
 
 elementView : Signal.Address Message -> (Int, Gif.Model) -> Html
 elementView address (id, model) =
-  Gif.view (Signal.forwardTo address (SubMsg id)) model
+    Gif.view (Signal.forwardTo address (SubMsg id)) model
 
 
 inputStyle : Attribute
 inputStyle =
-  style
-    [ ("width", "100%")
-    , ("height", "40px")
-    , ("padding", "10px 0")
-    , ("font-size", "2em")
-    , ("text-align", "center")
-    ]
+    style
+        [ ("width", "100%")
+        , ("height", "40px")
+        , ("padding", "10px 0")
+        , ("font-size", "2em")
+        , ("text-align", "center")
+        ]
 
 
 onEnter : Signal.Address a -> a -> Attribute
 onEnter address value =
     on "keydown"
-      (Json.customDecoder keyCode is13)
-      (\_ -> Signal.message address value)
+        (Json.customDecoder keyCode is13)
+        (\_ -> Signal.message address value)
 
 
 is13 : Int -> Result String ()
 is13 code =
-  if code == 13 then Ok () else Err "not the right key code"
+    if code == 13 then Ok () else Err "not the right key code"
